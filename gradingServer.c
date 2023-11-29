f36b2c6e920afa99b6c88903172dd0e9c901a63c
Akshay Patidar <akshaypatidar312@gmail.com>
2023-11-30 00:53:00 +0530

    Minor changes in grading server

--- gradingServer.c
+++ gradingServer.c
@@ -10,6 +10,8 @@
 #include <assert.h>
 #include <pthread.h>
 #include <sys/queue.h>
+#include <uuid/uuid.h>
+
 #define QUEUE_SIZE 30
 void *thread_function(void *arg);
 void enqueue(int *client_sockfd);
@@ -18,10 +20,12 @@ int *dequeue();
 const int BUFFER_SIZE = 1024;
 const int MAX_FILE_SIZE_BYTES = 4;
 int front = 0, rear = 0, count = 0, found = 0, taskCount = 0;
+char uuid_str[37]; 
 int *queue[QUEUE_SIZE];
 pthread_mutex_t queue_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t task_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t taskReady = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t uuid_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void error(char *msg)
 {
@@ -31,50 +35,50 @@ void error(char *msg)
 
 int recv_file(int sockfd, char *file_path)
 {
-    char buffer[BUFFER_SIZE];
-    bzero(buffer, BUFFER_SIZE);
-    FILE *file = fopen(file_path, "wb");
+	char buffer[BUFFER_SIZE];
+	bzero(buffer, BUFFER_SIZE);
+	FILE *file = fopen(file_path, "wb");
 
-    if (!file)
-    {
-        perror("Error opening file");
-        return -1;
-    }
+	if (!file)
+	{
+		perror("Error opening file");
+		return -1;
+	}
 
-    char file_size_bytes[MAX_FILE_SIZE_BYTES];
+	char file_size_bytes[MAX_FILE_SIZE_BYTES];
 
-    if (recv(sockfd, file_size_bytes, sizeof(file_size_bytes), 0) <= 0)
-    {
-        // perror("Error receiving file size");
-        fclose(file);
-        return -1;
-    }
+	if (recv(sockfd, file_size_bytes, sizeof(file_size_bytes), 0) <= 0)
+	{
+		// perror("Error receiving file size");
+		fclose(file);
+		return -1;
+	}
 
-    int file_size;
-    memcpy(&file_size, file_size_bytes, sizeof(file_size_bytes));
-    size_t bytes_read = 0, total_bytes_read = 0;
+	int file_size;
+	memcpy(&file_size, file_size_bytes, sizeof(file_size_bytes));
+	size_t bytes_read = 0, total_bytes_read = 0;
 
-    while (true)
-    {
-        bytes_read = recv(sockfd, buffer, BUFFER_SIZE, 0);
-        total_bytes_read += bytes_read;
+	while (true)
+	{
+		bytes_read = recv(sockfd, buffer, BUFFER_SIZE, 0);
+		total_bytes_read += bytes_read;
 
-        if (bytes_read <= 0)
-        {
-            perror("Error receiving file data");
-            fclose(file);
-            return -1;
-        }
+		if (bytes_read <= 0)
+		{
+			perror("Error receiving file data");
+			fclose(file);
+			return -1;
+		}
 
-        fwrite(buffer, 1, bytes_read, file);
-        bzero(buffer, BUFFER_SIZE);
+		fwrite(buffer, 1, bytes_read, file);
+		bzero(buffer, BUFFER_SIZE);
 
-        if (total_bytes_read >= file_size)
-            break;
-    }
+		if (total_bytes_read >= file_size)
+			break;
+	}
 
-    fclose(file);
-    return 0;
+	fclose(file);
+	return 0;
 }
 
 char *compile_command(char *request_id, char *programFile, char *execFile)
@@ -191,105 +195,46 @@ char *makeOutputDiffFileName(char *request_id)
 
 void *grader(int *sockfd)
 {
-    int newsockfd = *(int *)sockfd;
-    int n;
-    char *programFile, *execFile, *compileErrorFile, *runtimeErrorFile, *outputFile, *outputDiffFile;
-    programFile = makeProgramFileName((int)pthread_self());
-    execFile = makeExecFileName((int)pthread_self());
-    compileErrorFile = makeCompileErrorFileName((int)pthread_self());
-    runtimeErrorFile = makeRuntimeErrorFileName((int)pthread_self());
-    outputFile = makeOutputFileName((int)pthread_self());
-    outputDiffFile = makeOutputDiffFileName((int)pthread_self());
+	int newsockfd = *(int *)sockfd;
+	int n;
+	char *programFile, *execFile, *compileErrorFile, *runtimeErrorFile, *outputFile, *outputDiffFile;
+	// execFile = makeExecFileName((int)pthread_self());
+	// compileErrorFile = makeCompileErrorFileName((int)pthread_self());
+	// runtimeErrorFile = makeRuntimeErrorFileName((int)pthread_self());
+	// outputFile = makeOutputFileName((int)pthread_self());
+	// outputDiffFile = makeOutputDiffFileName((int)pthread_self());
 
-    char *comp_command, *r_command, *diff_command;
-    comp_command = compile_command((int)pthread_self(), programFile, execFile);
-    r_command = run_command((int)pthread_self(), execFile);
-    diff_command = output_diff_command((int)pthread_self());
+	// char *comp_command, *r_command, *diff_command;
+	// comp_command = compile_command((int)pthread_self(), programFile, execFile);
+	// r_command = run_command((int)pthread_self(), execFile);
+	// diff_command = output_diff_command((int)pthread_self());
 
-    while (1)
-    {
-        if (recv_file(newsockfd, programFile) != 0)
-        {
-            close(newsockfd);
-            return 0;
-        }
+		uuid_t uuid;
+		phtread_mutex_lock(&uuid_mutex);
+		uuid_generate(uuid);
+		uuid_unparse(uuid, uuid_str);
+		pthread_mutex_unlock(&uuid_mutex);
+		
+		programFile = makeProgramFileName(uuid_str);
 
-        if (system(comp_command) != 0)
-        {
-            // Handle compiler error
-            FILE *f = fopen(compileErrorFile, "rb");
-            fseek(f, 0, SEEK_END);
-            int len = ftell(f);
-            rewind(f);
-            char tempCompilerErrorBuffer[len];
-            size_t bytes_read = fread(tempCompilerErrorBuffer, 1, sizeof(tempCompilerErrorBuffer), f);
-            tempCompilerErrorBuffer[bytes_read] = '\0';
-            char compilerErrorBuffer[bytes_read + 16];
-            memset(compilerErrorBuffer, 0, sizeof(compilerErrorBuffer));
-            strcat(compilerErrorBuffer, "COMPILER ERROR\n");
-            strcat(compilerErrorBuffer, tempCompilerErrorBuffer);
-            
-            n = send(newsockfd, compilerErrorBuffer, sizeof(compilerErrorBuffer), 0);
-            if (n < 0)
-                error("ERROR writing to socket");
-			
-			fclose(f);
-        }
-        else if (system(r_command) != 0)
-        {
-            // Handle runtime time error
-            FILE *f = fopen(runtimeErrorFile, "r");
-            fseek(f, 0, SEEK_END);
-            int len = ftell(f);
-            rewind(f);
-            char tempRuntimeErrorBuffer[len];
-            size_t bytes_read = fread(tempRuntimeErrorBuffer, 1, sizeof(tempRuntimeErrorBuffer), f);
-            tempRuntimeErrorBuffer[bytes_read] = '\0';
-            char runtimeErrorBuffer[bytes_read + 15];
-            memset(runtimeErrorBuffer, 0, sizeof(runtimeErrorBuffer));
-            strcat(runtimeErrorBuffer, "RUNTIME ERROR\n");
-            strcat(runtimeErrorBuffer, tempRuntimeErrorBuffer);
+		if (recv_file(newsockfd, programFile) != 0)
+		{
+			close(newsockfd);
+			return 0;
+		}
 
-            n = send(newsockfd, runtimeErrorBuffer, sizeof(runtimeErrorBuffer), 0);
-            if (n < 0)
-                error("ERROR writing to socket");
-			
-			fclose(f);
-        }
-        else if (system(diff_command) != 0)
-        {
-            // Handle output difference
-            FILE *f = fopen(outputDiffFile, "r");
-            fseek(f, 0, SEEK_END);
-            int len = ftell(f);
-            rewind(f);
-            char tempDiffErrorBuffer[len];
-            size_t bytes_read = fread(tempDiffErrorBuffer, 1, sizeof(tempDiffErrorBuffer), f);
-            tempDiffErrorBuffer[bytes_read] = '\0';
-            char diffErrorBuffer[bytes_read + 18];
-            memset(diffErrorBuffer, 0, sizeof(diffErrorBuffer));
-            strcat(diffErrorBuffer, "OUTPUT DIFFRENCE\n");
-            strcat(diffErrorBuffer, tempDiffErrorBuffer);
-            
-            n = send(newsockfd, diffErrorBuffer, sizeof(diffErrorBuffer), 0);
-            if (n < 0)
-                error("ERROR writing to socket");
-			
-			fclose(f);
-        }
-        else
-        {
-            // Send success message
-            n = send(newsockfd, "PASS", sizeof("PASS"), 0);
-            if (n < 0)
-                error("ERROR writing to socket");
-        }
-    }
-    free(programFile);
-    free(execFile);
-    free(comp_command);
-    free(r_command);
-    close(newsockfd);
+		char response[90];
+		strcat(response,"I got your response and this is your request ID: ");
+		strcat(response,uuid_str);
+		n=send(newsockfd,response,sizeof(response),0);
+		if(n<0)
+		{
+			perror("Error sending response\n");
+			close(newsockfd);
+			return -1;
+		}
+		close(newsockfd);
+
 }
 
 int main(int argc, char *argv[])
@@ -305,10 +250,10 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Usage: %s <port no> <number of threads>\n", argv[0]);
 		exit(1);
 	}
-	
+
 	int MAX_THREADS = atoi(argv[2]);
 	pthread_t threadpool[MAX_THREADS];
-	
+
 	for (int i = 0; i < MAX_THREADS; i++)
 	{
 		pthread_create(&threadpool[i], NULL, thread_function, NULL);
@@ -318,7 +263,6 @@ int main(int argc, char *argv[])
 
 	if (sockfd < 0)
 		error("ERROR opening socket");
-
 
 	bzero((char *)&serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
